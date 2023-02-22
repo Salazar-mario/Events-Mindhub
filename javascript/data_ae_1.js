829e580df9728aeb5dfba755463c40353b16d7a2
agustin salazar <mario.agustin.salazar@gmail.com>
2023-02-22 20:16:17 -0300

    correciones js y media query

--- javascript/data_ae_1.js
+++ javascript/data_ae_1.js
@@ -1,8 +1,5 @@
 const titleSite = document.title;
-const loading = "Cargando...";
 const containerCategory = "category-container";
-const searchButton = document.getElementById("search");
-const inputSearch = document.getElementById("form-search");
 const checkArea = document.getElementById("category-container");
 
 
@@ -184,57 +181,6 @@ async function amazingEventsWeb() {
   console.log(data)
 
   let eventos = data.events;
-
-  // fecha de referencia ✔️
-  let fechaReferencia = data.currentDate;
-  console.log(fechaReferencia);
-
-  {
-    titleSite === "Amazing Events"
-      ? createCards(eventos, "contenedor-columnas")
-      : loading;
-  }
-   /*checkArea.addEventListener("change", () => {
-     let filteredCat = amazingChecks(eventos);
-     let filteredCards = filterCardsBySearch(filteredCat, inputSearch.value);
-     createCards(filteredCards, "contenedor-columnas");
-   });*/
-
-  //listener del inputSearch
-  searchButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    let filteredCards = filterCardsBySearch(eventos, inputSearch.value);
-    let filteredCat = amazingChecks(filteredCards);
-    createCards(filteredCat, "contenedor-columnas");
-  });
-
-  // Crear categorias
-  function creatCategory(array, divForm) {
-    array.forEach((cat) => {
-      let containerForm = document.getElementById(divForm);
-      let categorias = document.createElement("div");
-      categorias.className = "form-check";
-      categorias.innerHTML = `<label class="form-check-label cat-buttons" for="${cat}">
-         ${cat}
-         <input name="${cat}" class="form-check-input" type="checkbox" value="${cat}"
-             id="${cat}">
-         </label>`;
-      containerForm.appendChild(categorias);
-      return cat;
-    });
-  }
-
-  function filtrarCategory() {
-    let arrayCategoriasData = [];
-    eventos.map((e) => {
-      arrayCategoriasData.push(e.category);
-    });
-    let arrayCategorias = arrayCategoriasData.filter((cat, i) => {
-      return arrayCategoriasData.indexOf(cat) === i;
-    });
-    return arrayCategorias;
-  }
-
   // Crear Cards
   function createCards(amazing, contenedorColumnas) {
     let containerAbuelo = document.getElementById(contenedor - columnas);
@@ -256,68 +202,4 @@ async function amazingEventsWeb() {
     });
   }
 }
-
-
-function amazingChecks(amazing) {
-  //array de categorias de checkbox
-  const checkboxes = document.querySelectorAll("input[type='checkbox']");
-  let checkboxesChecked = Array.from(checkboxes)
-    .filter((check) => check.checked)
-    .map((e) => e.value);
-  if (checkboxesChecked.length > 0) {
-    let filteredCheckBox = amazing.filter((e) =>
-      checkboxesChecked.includes(e.category)
-    );
-    return filteredCheckBox;
-  }
-  return amazing;
-}
-
-//-- por boton de categoria Ya esta funcion no es necesaria linea 109
-function filterCards(amazing, check) {
-  let cardsFiltradas = amazing.filter((e) => check.includes(e.category));
-  return cardsFiltradas;
-}
-
-//                           arrayDB, textUsuario[]
-function filterCardsBySearch(amazing, textoSearch) {
-  let cardsfiltradasporcategoria = amazing.filter((e) =>
-    e.name.toLowerCase().includes(textoSearch.toLowerCase())
-  );
-
-  if (cardsfiltradasporcategoria.length == 0) {
-    noFound(textoSearch);
-    return [];
-  }
-  return cardsfiltradasporcategoria;
-}
-
-function noFound(textoSearch) {
-  let padre = document.getElementById("no-found");
-  let div = document.createElement("div");
-  div.className =
-    "container d-flex d-flex justify-content-center contact-div py-5";
-  div.innerHTML = `<div class="row">
-                        <div class="col-md-12">
-                            <div class="error-template">
-                                <h1>
-                                    Oops!</h1>
-                                <h2>
-                                    404 Not Found</h2>
-                                <div class="error-details">
-                                    Sorry,  ${textoSearch} , page not found!
-                                </div>
-                                <div class="error-actions">
-                                    <a href="#" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
-                                        Take Me Home </a><a href="http://www.jquery2dotnet.com" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
-                                </div>
-                            </div>
-                        </div>
-                         </div>`;
-  padre.appendChild(div);
-  setTimeout(() => {
-    div.remove(), location.reload();
-  }, 2000);
-}
-
 amazingEventsWeb();
